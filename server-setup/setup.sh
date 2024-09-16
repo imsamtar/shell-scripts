1ecf0d58cc0aa710883e14ee7fc1a7a8366f2875
Sameer Tariq <imsamtar@gmail.com>
2024-09-16 16:56:15 +0500

    update

--- server-setup/setup.sh
+++ server-setup/setup.sh
@@ -3,4 +3,6 @@
 curl -fsSL https://bun.sh/install | bash
 curl -fsSL -o server-setup.ts https://raw.githubusercontent.com/imsamtar/shell-scripts/main/server-setup/setup.ts
 
-echo Run "$HOME/.bun/bin/bun ./server-setup.ts"
+echo
+echo RUN "$HOME/.bun/bin/bun ./server-setup.ts"
+echo
