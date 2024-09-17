84262df063bfb38f4e39d5b4d1db971f259d21b5
Sameer Tariq <imsamtar@gmail.com>
2024-09-17 09:03:45 +0500

    update sh

--- server-setup/setup.sh
+++ server-setup/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y -qq unzip > /dev/null 2>&1
-curl -fsSL https://bun.sh/install | bash > /dev/null 2>&1
-curl -fsSL -o server-setup.ts https://raw.githubusercontent.com/imsamtar/shell-scripts/main/server-setup/setup.ts > /dev/null 2>&1
+sudo apt-get install -y -qq unzip >/dev/null 2>/dev/null
+curl -fsSL https://bun.sh/install | bash >/dev/null 2>/dev/null
+curl -fsSL -o server-setup.ts https://raw.githubusercontent.com/imsamtar/shell-scripts/main/server-setup/setup.ts >/dev/null 2>/dev/null
 
 echo
 echo RUN "$HOME/.bun/bin/bun ./server-setup.ts"
