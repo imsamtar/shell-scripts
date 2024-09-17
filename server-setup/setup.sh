fddf5bd7e2fcdec427b92958f4d711b12056ca59
Sameer Tariq <imsamtar@gmail.com>
2024-09-17 09:05:34 +0500

    forward curl progress to null

--- server-setup/setup.sh
+++ server-setup/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y -qq unzip >/dev/null 2>/dev/null
-curl -fsSL https://bun.sh/install | bash >/dev/null 2>/dev/null
+curl -fsSL https://bun.sh/install > /dev/null | bash >/dev/null 2>/dev/null
 curl -fsSL -o server-setup.ts https://raw.githubusercontent.com/imsamtar/shell-scripts/main/server-setup/setup.ts >/dev/null 2>/dev/null
 
 echo
